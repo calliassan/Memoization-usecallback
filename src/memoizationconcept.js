c15828e901dc5b8a93f00fac053eb59e1647aa54
Ashish Bhattarai <ashishbhattarai49@gmail.com>
2024-10-08 11:46:27 +0530

    some additional concepts

--- src/memoizationconcept.js
+++ src/memoizationconcept.js
@@ -1,11 +1,15 @@
+//defining sum function(1st)
 const sum = (a, b) => {
   console.log("called with params", { a, b });
   return a + b;
 };
+//when we see below, memoize takes a function called sum so, pass function fn as a parameter. when we
+//see console, we have called memoizedsum and passed(1,2) as argument. this means we have to catch those arguments inside memoize
+//while returning a function.
 const memoize = (fn) => {
-  const cache = {};
+  const cache = {}; //(3rd)
   return function (...args) {
-    const key = JSON.stringify(args);
+    const key = JSON.stringify(args); //key is arguments
     if (cache[key]) {
       return cache[key];
     }
@@ -14,6 +18,7 @@ const memoize = (fn) => {
     return res;
   };
 };
+//defining a new fuction memoized =sum that calls another function memoize(2nd)
 const memoizedsum = memoize(sum);
 console.log(memoizedsum(1, 2));
 console.log(memoizedsum(1, 2));
